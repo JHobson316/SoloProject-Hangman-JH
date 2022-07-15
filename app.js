46020b1badc02db6761ca3a231cb2abbd7a8f0a9
Jordan Hobson <hobsoncjordan@gmail.com>
2022-07-14 22:24:12 -0500

    Completed main requirements, not alot of css

--- app.js
+++ app.js
@@ -11,25 +11,33 @@ let start = document.getElementById("start");
 let reset = document.getElementById("reset");
 let guess = document.getElementById("guess");
 let select = document.getElementById("select");
-let usedLetters = document.getElementById("usedLetters");
+let usedLetters = document.getElementById("missedLetters");
 let wordDisplay = document.getElementById("wordDisplay");
 let lives = 6;
+let keyboard = document.getElementById("keyboard");
+let letter = document.getElementsByClassName("letter");
+let canvas = document.getElementById("canvas");
 
 // Variable declaration
 let sports = "baseball basketball football badminton fencing jousting volleyball tennis cycling hurdles diving dodgeball nascar".split(" ");
 let tvShows = ["bobs-burgers", "the-good-place", "the-walking-dead",
     "the young-and-the-restless", "drake-and-josh", "icarly", "the-boys"];
-let movies = ["baby driver", "the-avengers", "pulp-fiction", "inception",
+let movies = ["baby-driver", "the-avengers", "pulp-fiction", "inception",
     "interstellar", "forrest-gump", "parasite", "aliens", "terminator", "soul",
     "moana", "lion-king", "the-mitchells-vs-the-machines"];
 let allArrays = [];
+let letterArray = [];
+let missedLetters = [];
+let blanks = [];
+let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
 
 // Setting up extra arrays pulled from python file
 
 let p = "bulbasaur ivysaur venusaur charmander charmeleon \
 charizard squirtle wartortle blastoise caterpie metapod \
 butterfree weedle kakuna beedril pidgey pidgeotto pidgeot \
-rattatta raticate spearow fearow ekans arbok pikachu\
+rattatta raticate spearow fearow ekans arbok pikachu \
 raichu sandshrew sandslash nidoran nidorina \
 nidoqueen nidorino nidoking".split(" ");
 
@@ -50,12 +58,88 @@ allArrays.push(p);
 allArrays.push(o);
 allArrays.push(u);
 console.log(allArrays);
+//Canvas Sketching
+if (canvas.getContext) {
+    // if true
+    // Sketching out gallows
+    const ctx = canvas.getContext('2d');
+    ctx.beginPath()
+    ctx.rect(50, 380, 200, 150)
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(70, 380);
+    ctx.lineTo(70, 120);
+    ctx.lineTo(180, 120);
+    ctx.lineTo(180, 200);
+    ctx.lineTo(150, 200);
+    ctx.lineTo(150, 150);
+    ctx.lineTo(105, 150);
+    ctx.lineTo(105, 380);
+    ctx.fill();
+}
 
 
-// Insporation and solution from https://github.com/SimonLeclere/discord-hangman
-// Credit where credit is due
+// Using a canvas chart to display the pictures
 // Hangman Pictures (Currently very much not working)
-function selectWord(){
+function hangmanBody(lives) {
+    if (lives == 5) {
+        if (canvas.getContext) {
+            const ctx = canvas.getContext('2d');
+            ctx.beginPath();
+            ctx.arc(165, 225, 25, 0, 2 * Math.PI);
+            ctx.stroke();
+        }
+    }
+    if (lives == 4) {
+        if (canvas.getContext) {
+            const ctx = canvas.getContext('2d');
+            ctx.beginPath();
+            ctx.moveTo(165, 250);
+            ctx.lineTo(165, 310);
+            ctx.stroke();
+        }
+    }
+    if (lives == 3) {
+        if (canvas.getContext) {
+            const ctx = canvas.getContext('2d');
+            ctx.beginPath();
+            ctx.moveTo(165, 250);
+            ctx.lineTo(195, 275);
+            ctx.stroke();
+        }
+    }
+    if (lives == 2) {
+        if (canvas.getContext) {
+            const ctx = canvas.getContext('2d');
+            ctx.beginPath();
+            ctx.moveTo(165, 250);
+            ctx.lineTo(135, 275);
+            ctx.stroke();
+        }
+    }
+    if (lives == 1) {
+        if (canvas.getContext) {
+            const ctx = canvas.getContext('2d');
+            ctx.beginPath();
+            ctx.moveTo(165, 310);
+            ctx.lineTo(195, 335);
+            ctx.stroke();
+        }
+    }
+    if (lives == 0) {
+        if (canvas.getContext) {
+            const ctx = canvas.getContext('2d');
+            ctx.beginPath();
+            ctx.moveTo(165, 310);
+            ctx.lineTo(135, 335);
+            ctx.stroke();
+        }
+        alert("Game over! The secret word was " + letterArray.join("") + "!")
+    }
+}
+// Functions
+
+function selectWord() {
     wordDisplay.innerHTML = " ";
     let chosenArray = select.value;
     console.log(select.value); // Works
@@ -63,40 +147,70 @@ function selectWord(){
     console.log(selectedArray) // Works
     let randIndex = Math.floor(Math.random() * selectedArray.length);
     let selectedWord = selectedArray[randIndex];
-    let letterArray = selectedWord.split("");
+    letterArray = selectedWord.split("");
     console.log(selectedWord.split(""));
     displayWord(letterArray);
 }
 
-
-// Functions
+// Resets the html page and selects a new word
 reset.addEventListener('click', function (e) {
+    usedLetters.innerHTML = "Wrong Letters: ";
     e.preventDefault();
+    lives = 6;
     selectWord();
 });
-start.addEventListener('click', function (e) {
-    e.preventDefault();
-    if (letterArray.includes(guess)){
-        
-    }
-
-})
 function displayWord(wordArray) {
-    let blanks = [];
+    blanks = [];
     wordArray.forEach(i => {
-        if (wordArray[i]=="-"){
-            blanks.push("-");
-        }
-        else{
+        if (alphabet.includes(i)) {
             blanks.push("_");
         }
+        else if (alphabet.includes(i) == false) {
+            blanks.push("-");
+            console.log("Help");
+        }
     })
-    console.log(blanks);
-    for (let i = 0; i<blanks.length; i++){
-    wordDisplay.innerHTML = wordDisplay.innerHTML + blanks[i] +" ";
+    actualDisplay(blanks);
+}
+function actualDisplay(array) {
+    console.log(array);
+    wordDisplay.innerHTML = " "
+    for (let i = 0; i < array.length; i++) {
+        wordDisplay.innerHTML = wordDisplay.innerHTML + array[i] + " ";
     }
 }
 selectWord();
-//function start(){
-
-//}
+// Event listeners for the entire class of buttons
+for (const character of letter) {
+    character.addEventListener('click', function (e) {
+        e.preventDefault();
+        console.log("Bop");
+        console.log(character.value);
+        if (letterArray.includes(character.value)) {
+            for (let i = 0; i < letterArray.length; i++) {
+                if (letterArray[i] == character.value) {
+                    blanks.splice(i, 1, letterArray[i]);
+                    // Tracking both arrays 
+                    console.log("Word: " + letterArray);
+                    console.log("Progress: " + blanks);
+                    actualDisplay(blanks);
+                    // Check for a game win here
+                    let yes = 0;
+                    for (let i = 0; i < blanks.length; i++) {
+                        if (blanks[i] == letterArray[i]) {
+                            yes++
+                            if (yes == letterArray.length) {
+                                alert("Congratulations! You won!")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        else {
+            usedLetters.innerHTML = usedLetters.innerHTML + character.value + ", ";
+            --lives;
+            hangmanBody(lives);
+        }
+    })
+}
